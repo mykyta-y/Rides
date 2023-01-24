9f6e11539b14d42bf46d6063db036a53d5a28741
mykyta-y <mykyta.y@ibm.com>
2023-01-24 10:44:23 -0400

    Initial Commit

---
+++ Rides/RidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RidesApp.swift
+//  Rides
+//
+//  Created by Mykyta Yefremchenkov on 2023-01-24.
+//
+
+import SwiftUI
+
+@main
+struct RidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
